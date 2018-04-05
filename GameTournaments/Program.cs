c15a03a95914b7e10f1e5608e812f07745076a6b
Кирилл Абраменко <wf.otec@mail.ru>
2018-04-05 19:05:51 +0700

    remove excess

--- GameTournaments/Program.cs
+++ GameTournaments/Program.cs
@@ -1,38 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Windows.Forms;
 
 namespace GameTournaments
 {
     class Program
     {
+        static List<Tournament> listTournaments = new List<Tournament>();
         static void Main()
         {
-            //List<Judge> listJudge= new List<Judge>();
-            //Judge judge1 = new Judge("Alex", "Lex", 20, "Dota");
-            //Player[] Players;
-            //listJudge.Add(judge1);
-            //judge1.ShowInfo();
-            //int count = Convert.ToInt32(Console.ReadLine());
-            //Players = CreateListPlayers(count);
-            //ShowListPlayers(Players);
-            /* Team asdf = CreateTestTeam("Komanda", GameDiscipline.Dota);
-             Team asdfg = CreateTestTeamNull("KomandaB", GameDiscipline.Cs);
-             Tournament tour = new Tournament("Turnir", 128000);
-             tour.AddTeam(asdf);
-             tour.AddTeam(asdfg);
-             Console.WriteLine(tour.ToString());
-             Console.WriteLine(asdf.ToString());
-             Console.WriteLine(asdfg.ToString());
-             Interface adminCreateTour = new Interface();
-             Tournament tournamentTest = adminCreateTour.CreateTournament();
-            // Console.WriteLine(tourna.ToString());*/
-            var listTournaments = new List<Tournament>();
             var mainInterface = new Interface();
-            //listTournaments.Add(mainInterface.CreateTournament());
-            //listTournaments[0] = listTournaments[0].AddTeam(mainInterface.AddTeam(listTournaments[0], 1));
-            //listTournaments[0] = listTournaments[0].AddTeam(mainInterface.AddTeam(listTournaments[0], 2));
-            //Console.WriteLine(listTournaments[0].ToString());
             int caseSwitch = -1;
             while (caseSwitch != 0)
             {
@@ -45,8 +23,17 @@ namespace GameTournaments
                 switch (caseSwitch)
                 {
                     case 1:
-                        Console.WriteLine("Case 1");
-                        listTournaments.Add(mainInterface.CreateTournament());
+                        //listTournaments.Add(mainInterface.CreateTournament());
+                        Console.Write("Введите название турнира: ");
+                        string tourName = Console.ReadLine();
+                        Console.Write("Введите призовой фонд (валюта $): ");
+                        int prizePool = Convert.ToInt32(Console.ReadLine());
+                        var tournament = new Tournament(tourName, prizePool);
+                        Console.WriteLine("Выберите дисциплину из списка:");
+                        Console.WriteLine("0 - Dota 2");
+                        Console.WriteLine("1 - CS:GO");
+                        var idDiscipline = Convert.ToInt32(Console.ReadLine());
+                        AddTournament(tourName, prizePool, idDiscipline);
                         Console.WriteLine("Введите количество команд: ");
                         int countPlayers = 0;
                         int countTeams = Convert.ToInt32(Console.ReadLine());
@@ -109,41 +96,11 @@ namespace GameTournaments
             Console.ReadLine();
         }
 
-        public static Team CreateTestTeam(string teamName, GameDiscipline discipline)
+        public static void AddTournament(string tourName, int prizePool, int idDiscipline)
         {
-            var team = new Team(teamName);
-            team.AddPlayer(new Player("Kirill", "Abramenko", "DecadE", new DateTime(1998, 08, 12)));
-            team.AddPlayer(new Player("Danil", "Ivanov", "Hook", new DateTime(1995, 12, 05)));
-            return team;
-        }
-        public static Team CreateTestTeamNull(string teamName, GameDiscipline discipline)
-        {
-            var team = new Team(teamName);
-            return team;
-        }
-
-        public static Player[] CreateListPlayers(int count)
-        {
-            Player[] players = new Player[count];
-            for (int i = 0; i < count; i++)
-            {
-                Console.Write($"Заполнение информации. Игрок номер {i + 1}");
-                Console.Write($"#Имя:");
-                players[i] = new Player { Name = Console.ReadLine() };
-                Console.Write($"#Фамилия:");
-                players[i].Surname = Console.ReadLine();
-                Console.Write($"#Никнейм:");
-                players[i].NickName = Console.ReadLine();
-            }
-            return players;
-        }
-        public static void ShowListPlayers(Player[] players)
-        {
-            for (int i = 0; i < players.Length; i++)
-            {
-                Console.Write($"Игрок номер {i + 1}");
-                Console.WriteLine($"{players[i].Name}");
-            }
+            var tournament = new Tournament(tourName, prizePool);
+            tournament.GameDiscipline = (GameDiscipline)idDiscipline;
+            listTournaments.Add(tournament);
         }
     }
 }
